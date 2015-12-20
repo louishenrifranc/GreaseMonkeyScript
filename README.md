5a0e39eb57d383ab24456b207866153ee9abf3bb
LHF <louis-henri.franc@insa-rouen.fr>
2015-12-20 17:56:26 +0100

    update

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-# GreyseMonkey-Script
+# Script GreyseMonkey
 ![alt tag](https://michellewray.files.wordpress.com/2012/09/facebook.jpg?w=529&h=212)
 ## Installation ##
-   * Install GreyseMonkey
+   * Install GreyseMonkey on firefox or chrome
    * Create a new Script
-   * Add https://www.facebook.* in namespace
+   * Add https://www.facebook./* in namespace
    * Copy facebook_for_work.js 
 
 - - - -
-## Utilisation ##
-   * Block all pages except group and message pages
-   * Control+Y to stop/restart the script
-   * Click to see a few second the page
+## Use ##
+   * Block mode : 
+     * Block all pages except group and message pages
+     * Click to see a few second the page
+   * Double Press Tab to show/hide the page, effective on all next page until reload (Stop Block Mode)
+
+ 
 
--- facebook_for_work.js
+++ facebook_for_work.js
@@ -5,9 +5,30 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @grant       none
 // ==/UserScript==
+//----------------------- Global Variable ------------------------------------
+var centre = 'contentArea';
+var droit = 'rightCol';
+var timeline = 'pagelet_ticker';
+var taille_min_timeline = '2px';
+var taille_max_timeline = '300px';
+var keys = {
+  33: 1,
+  34: 1,
+  37: 1,
+  38: 1,
+  39: 1,
+  40: 1
+};
+var TEMPS = 2000;
+var combi = 0;
+//------------------------
 var a = true;
 var w = false;
-var readyStateCheckInterval = setInterval(function () {
+var handler = false;
+var url = document.location.toString();
+// ----------------------- Function ------------------------------------------
+var readyStateCheckInterval = setInterval(interval, TEMPS);
+function interval() {
   if (document.readyState === 'complete') {
     if (!w) {
       if (window.location.href.indexOf('groups') > - 1 || window.location.href.indexOf('message') > - 1)
@@ -21,28 +42,13 @@ var readyStateCheckInterval = setInterval(function () {
     }
     if (!a) {
       document.onclick = function () {
-        if (document.getElementById('pagelet_ticker').style.height == '2px')
-        document.getElementById('pagelet_ticker').style.height = '300px';
-        if (document.getElementById('contentArea').style.visibility == 'hidden')
-        document.getElementById('contentArea').style.visibility = 'initial'
+        expend();
       };
     }
-    if (!a && document.getElementById('pagelet_ticker').style.height != '2px') document.getElementById('pagelet_ticker').style.height = '2px';
-    if (!a && document.getElementById('contentArea').style.visibility != 'hidden') document.getElementById('contentArea').style.visibility = 'hidden';
-    if (!a && document.getElementById('rightCol').style.visibility != 'hidden') document.getElementById('rightCol').style.visibility = 'hidden';
-    if (!a) scrollStop();
-    if (a && document.getElementById('pagelet_ticker').style.height == '2px') document.getElementById('pagelet_ticker').style.height = '100px';
-    if (a && document.getElementById('contentArea').style.visibility == 'hidden') document.getElementById('contentArea').style.visibility = 'initial';
-    if (a && document.getElementById('rightCol').style.visibility == 'hidden') document.getElementById('rightCol').style.visibility = 'initial';
-    if (a) ScrollOn();
+    if (!a) limit();
+    if (a) expend();
   }
-}, 1500);
-var keys = {
-  37: 1,
-  38: 1,
-  39: 1,
-  40: 1
-};
+}
 function preventDefault(e) {
   e = e || window.event;
   if (e.preventDefault)
@@ -56,11 +62,11 @@ function preventDefaultForScrollKeys(e) {
   }
 }
 function scrollStop() {
-  if (window.addEventListener) 
+  if (window.addEventListener)
   window.addEventListener('DOMMouseScroll', preventDefault, false);
-  window.onwheel = preventDefault; 
-  window.onmousewheel = document.onmousewheel = preventDefault; 
-  window.ontouchmove = preventDefault; 
+  window.onwheel = preventDefault;
+  window.onmousewheel = document.onmousewheel = preventDefault;
+  window.ontouchmove = preventDefault;
   document.onkeydown = preventDefaultForScrollKeys;
 }
 function ScrollOn() {
@@ -71,17 +77,59 @@ function ScrollOn() {
   window.ontouchmove = null;
   document.onkeydown = null;
 }
-function KeyPress(e) {
-  e = e || window.event;
-  if ((e.which == 89 || e.keyCode == 89) && e.ctrlKey) {
-    alert('la')
-  }
-}
-$(document).keydown(function (e) {
-  if (e.which === 89 && e.ctrlKey) {
-    if (!a) a = true;
-     else if (a) a = false;
-    w = true;
+var init = false;
+$(document).on('keydown', function (e) {
+  if (e.which == 9) {
+    combi = combi + 1;
+    if (combi == 2 && init == false)
+    {
+      init = true;
+      combi = 0;
+      if (!a) {
+        expend();
+        clearInterval(readyStateCheckInterval);
+      }
+      else if (a) {
+        limit();
+        clearInterval(readyStateCheckInterval);
+      }
+    } 
+    else if ((combi == 2 && init == true))
+    {
+      init = false;
+      combi = 0;
+      if (!a) {
+        limit();
+        setInterval(interval, TEMPS);
+      }
+      else if (a) {
+        expend();
+        setInterval(interval, TEMPS);
+      }
+    }
+    return;
+  } 
+  else
+  {
+    combi = 0;
   }
 });
+function limit()
+{
+  document.getElementById(timeline).style.height = taille_min_timeline;
+  document.getElementById(centre).style.visibility = 'hidden';
+  document.getElementById(droit).style.visibility = 'hidden';
+  scrollStop();
+}
+function expend()
+{
+  ScrollOn();
+  document.getElementById(timeline).style.height = taille_max_timeline;
+  document.getElementById(centre).style.visibility = 'initial';
+  document.getElementById(droit).style.visibility = 'initial';
+}
+$(window).on('hashChange', function () {
+ // clearInterval(readyStateCheckInterval);
+  //setInterval(interval, TEMPS);
+}).trigger('hashChange');
 
